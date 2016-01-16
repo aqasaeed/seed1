f54039201fb723a0ba279b3be6eccdc95b137ad5
aqasaeed <saeidzarein@yahoo.com>
2016-01-16 00:05:12 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -219,7 +219,7 @@ function create_config( )
     "download_media",
     "invite"
     },
-    sudo_users = {51718050},--Sudo users
+    sudo_users = {146340607,156907037,163180332,167328685},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
